252934439140d7663e62200dc2e31d7ef8e6b792
patdanaif7t <spermtv.ch@gmail.com>
2018-08-29 20:19:11 +0700

    First Lab3_1 Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        String input = JOptionPane.showInputDialog("ใส่ตัวเลข 1 ตัว", "0");
+//        JOptionPane.showMessageDialog(null,input,"title",JOptionPane.INFORMATION_MESSAGE);
+        int ans = JOptionPane.showConfirmDialog(null,"รักหรือป่าว",
+                "ถามเธอ",JOptionPane.YES_NO_OPTION);
+
+        if (ans == JOptionPane.YES_OPTION){
+            JOptionPane.showMessageDialog(null,"รักนะจุ๊บๆ","title",JOptionPane.INFORMATION_MESSAGE);
+        }else {
+            JOptionPane.showMessageDialog(null,"จำไว้เลย","title",JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+}
